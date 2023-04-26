e5fc2662a1c7be49da0742ba814d37d907dd1b50
fltea <xm1025@foxmail.com>
2023-04-26 20:25:05 +0800

    feat: pinia 配置

--- vue3-ts-cms/src/main.ts
+++ vue3-ts-cms/src/main.ts
@@ -3,7 +3,9 @@ import 'normalize.css'
 import '@/assets/css/index.less'
 import App from './App.vue'
 import router from './router'
+import pinia from './store'
 
 const APP = createApp(App)
 APP.use(router)
+APP.use(pinia)
 APP.mount('#app')
