d3e144b1dac3aa72cd4c1058ec3305e3bbedae82
fltea <xm1025@foxmail.com>
2023-04-26 20:13:02 +0800

    feat: 新增 404 页面

---
+++ vue3-ts-cms/src/pages/404.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <section class="not-found">
+    <h2>Not-Found</h2>
+  </section>
+</template>
+
+<style lang="less" scoped>
+.not-found {
+  color: red;
+}
+</style>
