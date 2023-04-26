7c13b861a055e8be286d216c7db37241106ce52a
fltea <xm1025@foxmail.com>
2023-04-26 20:12:42 +0800

    feat: 新增 login 页面

---
+++ vue3-ts-cms/src/pages/login.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <section class="login">
+    <h2>login</h2>
+  </section>
+</template>
+
+<style lang="less" scoped>
+.login {
+  color: red;
+}
+</style>
