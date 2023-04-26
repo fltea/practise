9e6b7786bcdcf2d585d96d741e025bffd8e545c1
fltea <xm1025@foxmail.com>
2023-04-26 20:12:54 +0800

    feat: 新增 home 页面

---
+++ vue3-ts-cms/src/pages/home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <section class="home">
+    <h2>home</h2>
+  </section>
+</template>
+
+<style lang="less" scoped>
+.home {
+  color: red;
+}
+</style>
