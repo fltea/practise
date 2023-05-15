8e3611371d78b423589f1b53ea4cd0024f91a4a5
fltea <xm1025@foxmail.com>
2023-05-15 20:11:31 +0800

    feat: var const

--- gos/example/vargo.go
+++ gos/example/vargo.go
@@ -1,23 +1,24 @@
-package main 
+//go:build ignore
+
+package main
 
 import (
 	"fmt"
 	"math"
 )
 
-func main () {
+func main() {
 	var a = "initial"
-	var b,c = 1,2
+	var b, c = 1, 2
 	var d = true
 	var e float64
 	f := float32(e)
 	g := a + "foo"
-	fmt.Println(a,b,c,d,e,f)
+	fmt.Println(a, b, c, d, e, f)
 	fmt.Println(g)
 
-	const s string ="constant"
+	const s string = "constant"
 	const h = 5000000000
 	const i = 3e20 / h
-	fmt.Println(s,h,i, math.Sin(h), math.Sin(i))
+	fmt.Println(s, h, i, math.Sin(h), math.Sin(i))
 }
-
